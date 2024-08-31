d54087aa4144d562094e8c1c942c71e6770ce5b0
jkkj <rhythmgulati11@gmail.com>
2024-09-01 00:00:13 +0530

    second

--- src/components/RedirectCard.jsx
+++ src/components/RedirectCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import "./RedirectCard.css"
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
-function RedirectCard({name,path}) {
+
+
+function RedirectCard({name,path,icon}) {
   return (
-    <div className="h-[35vh] w-[90%] md:w-[35%] bg-slate-300 bg-white/5 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-sm rounded-lg border border-white/20 flex flex-col text-white items-center justify-around ">
+    <div className="h-[35vh] w-[90%] md:w-[35%] bg-slate-300 bg-white/5 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-sm rounded-lg border border-white/20 flex flex-col text-white items-center justify-around hover:scale-105 transition-all duration-1000">
+        <div className="flex gap-5">
+        <FontAwesomeIcon icon={icon} className="text-3xl" />
         <p className="text-2xl">{name}</p>
-        <Link to={`/${path}`} className="bg-slate-300 bg-white/5 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-sm rounded-lg border border-white/20 px-7 py-2 ">Start</Link>
+        </div>
+        <Link to={`/${path}`} className="bg-slate-300 bg-white/5 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-sm rounded-lg border border-white/20 px-7 py-2 hover:bg-slate-400 ">Start</Link>
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import RedirectCard from '../components/RedirectCard'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import RedirectCard from '../components/RedirectCard';
+import { faCircleQuestion,faSpellCheck } from '@fortawesome/free-solid-svg-icons'; 
 
 function Homepage() {
-
   const values = [
-    {name:"QUIZ",path:"/quiz"},
-    {name:"CROSSWORD",path:"/quiz"},
-    {name:"QUIZ",path:"/quiz"},
-    {name:"SCRAMBLE",path:"/scramble"}
-]
+    { name: "QUIZ", path: "/quiz", icon: faCircleQuestion}, 
+    { name: "CROSSWORD", path: "/quiz", icon:faSpellCheck  },
+    { name: "SNVI", path: "/quiz",icon:faSpellCheck },
+    { name: "SCRAMBLE", path: "/scramble" }
+  ];
+
   return (
     <div className="bg-[url('/img/5413332.jpg')] h-screen bg-center font-medium text-xl">
-      <Navbar/>
-      <div className="h-[84vh] my-2 flex flex-wrap justify-center gap-4 items-center p-10">
-      {values.map((item)=>(
-        <RedirectCard name={item.name} path={item.path} />
-    )
-      )}
+      <Navbar />
+      <div className="h-[84vh] my-2 flex flex-wrap justify-center gap-8 items-center p-10">
+        {values.map((item, i) => (
+          <RedirectCard key={i} name={item.name} path={item.path} icon={item.icon} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
